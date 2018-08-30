33ae8971376989d12a677c7bffb8cce67c270f71
Thomas Mathisen <thomlmath@gmail.com>
2018-08-30 16:40:34 +0200

    Added some kiting skills and defend home

--- zerg_agent.py
+++ zerg_agent.py
@@ -24,13 +24,22 @@ import enum
 #
 # Todos 
 # Start with only one overlord 
-# Upgrades
 # Invis units
-# Improve focus and kiting back and forth 
 # Fight units on higher ground
 # On lost base build army to withstand another attack
-# If at home, attack
+# Scout
+# If early aggression decrese droneArmyRatio #important #machineLearning
+# Implement 70-30 attack for when maxed out 
+# Bring overlords into fights for vision
+# Add more units it can build
+# Don't walk past enemies
 #
+# Kind of done
+# Upgrades
+# 
+# Done
+# Improve focus and kiting back and forth 
+# If at home, attack
 ###
 
 
@@ -38,17 +47,10 @@ import enum
 class LingLoverBot(BaseBot):
 
     units_to_ignore = [DRONE, SCV, PROBE, EGG, LARVA, OVERLORD, OVERSEER, OBSERVER, BROODLING, INTERCEPTOR, MEDIVAC, CREEPTUMOR, CREEPTUMORBURROWED, CREEPTUMORQUEEN, CREEPTUMORMISSILE]
-    roachHydraRatio = 0.6 # 70% roaches
-    droneArmyRatio = 0.4
+    roachHydraRatio = 0.7 # 70% roaches
+    droneArmyRatio = 0.3
     army_size_minimum = 20
     start_location = None
-
-
-    def select_target(self):
-        if self.known_enemy_structures.exists:
-            return random.choice(self.known_enemy_structures).position
-
-        return self.enemy_start_locations[0]
 
     async def on_step(self, iteration):
         self.combinedActions = []
@@ -70,13 +72,14 @@ class LingLoverBot(BaseBot):
         if iteration % 25 == 0:
             await self.distribute_workers()
 
-        self.trainUnits()
+        await self.trainUnits()
         await self.handleBase()
 
         await self.handleQueen()
         
-        self.move_army()
+        await self.handleUpgrades()
 
+        self.move_army()
         await self.do_actions(self.combinedActions)
 
 
@@ -86,7 +89,7 @@ class LingLoverBot(BaseBot):
     # Only run once at game start
     async def startUp(self):
         # Say hello!
-        await self.chat_send("(probe)(pylon)(cannon)(cannon)(gg)")
+        #await self.chat_send("(drone)(roach)(roach)(roach)(roach)(roach)(roach)(roach)(roach)(roach)(roach)(gg)")
 
         # Save base locations for later
         self.start_location = self.units(HATCHERY).first.position
@@ -103,35 +106,33 @@ class LingLoverBot(BaseBot):
         return self.supply_left >= self._game_data.units[unit_type.value]._proto.food_required
 
 
-    def trainUnits(self):
+    async def trainUnits(self):
         larvae = self.units(LARVA)
 
         if larvae.exists:
             # Make sure we have supply 
             if self.supply_left < 2 + self.supply_used*0.1 and self.already_pending(OVERLORD) < 2 and self.supply_cap < 200:
                 if self.can_afford(OVERLORD) and larvae.exists:
-                    self.combinedActions.append(larvae.random.train(OVERLORD))
-                    return
+                    await self.do(larvae.random.train(OVERLORD))
 
             # Make drone if we have sufficently large army
-            if self.units(DRONE).amount < 70 and self.units(DRONE).amount < self.bases.amount * 16 and self.army.amount + 10 >= self.units(DRONE).amount * self.droneArmyRatio:
+            elif self.units(DRONE).amount < 70 and self.units(DRONE).amount < self.bases.amount * 16 and self.army.amount + 10 >= self.units(DRONE).amount * self.droneArmyRatio:
                 if self.can_afford(DRONE) and self.can_feed(DRONE):
-                    self.combinedActions.append(larvae.random.train(DRONE))
+                    await self.do(larvae.random.train(DRONE))
 
             # Make lings until roaches are available
             elif self.units(SPAWNINGPOOL).ready and self.can_feed(ZERGLING) and not self.units(ROACHWARREN).ready:
                 if larvae.exists and self.can_afford(ZERGLING):
-                    self.combinedActions.append(larvae.random.train(ZERGLING))
+                    await self.do(larvae.random.train(ZERGLING))
 
             # Make roaches until hydra is possible or the ratio is in favor of hydra
             elif self.units(ROACHWARREN).ready and (not self.units(HYDRALISKDEN).ready or self.units(HYDRALISK).amount > 0 and self.units(ROACH).amount/self.units(HYDRALISK).amount > self.roachHydraRatio):
                 if self.can_afford(ROACH) and self.can_feed(ROACH):
-                    self.combinedActions.append(larvae.random.train(ROACH))
+                    await self.do(larvae.random.train(ROACH))
 
             elif self.units(HYDRALISKDEN).ready and (self.units(HYDRALISK).amount <= 0 or self.units(ROACH).amount/self.units(HYDRALISK).amount <= self.roachHydraRatio):
                 if self.can_afford(HYDRALISK) and self.can_feed(HYDRALISK):
-                    self.combinedActions.append(larvae.random.train(HYDRALISK))
-
+                    await self.do(larvae.random.train(HYDRALISK))
 
 
     async def handleQueen(self):
@@ -192,9 +193,9 @@ class LingLoverBot(BaseBot):
                 await self.build(SPAWNINGPOOL, near=self.hq)
         
         # Make extractors
-        elif (self.already_pending(SPAWNINGPOOL) or self.units(SPAWNINGPOOL).exists) and self.units(EXTRACTOR).amount < math.floor(1.5 * self.bases.amount) and not self.already_pending(EXTRACTOR) and self.units(DRONE).amount > 15:
+        elif (self.already_pending(SPAWNINGPOOL) or self.units(SPAWNINGPOOL).exists) and self.units(EXTRACTOR).amount < math.floor(1.45 * self.bases.amount) and not self.already_pending(EXTRACTOR) and self.units(DRONE).amount > 15 + 10 * self.units(EXTRACTOR).amount:
             if self.can_afford(EXTRACTOR):
-                target = self.state.vespene_geyser.closest_to(self.bases.random)
+                target = self.state.vespene_geyser.closest_to(self.bases.ready.random)
                 drone = self.workers.closest_to(target)
                 err = self.combinedActions.append(drone.build(EXTRACTOR, target))
 
@@ -212,6 +213,42 @@ class LingLoverBot(BaseBot):
         elif self.units(LAIR).ready and not (self.units(HYDRALISKDEN).exists or self.already_pending(HYDRALISKDEN)):
             if self.can_afford(HYDRALISKDEN):
                 await self.build(HYDRALISKDEN, near=self.hq)
+
+        elif self.units(ROACHWARREN).ready and self.units(SPAWNINGPOOL).ready and not (self.units(EVOLUTIONCHAMBER).exists or self.already_pending(EVOLUTIONCHAMBER)):
+            if self.can_afford(EVOLUTIONCHAMBER):
+                await self.build(EVOLUTIONCHAMBER, near=self.hq)
+
+
+    async def handleUpgrades(self):
+        if self.units(EVOLUTIONCHAMBER).ready.exists:
+            evo = self.units(EVOLUTIONCHAMBER).first
+            abilities = await self.get_available_abilities(evo)
+            
+            if evo.noqueue and len(abilities) > 0 and RESEARCH_ZERGGROUNDARMORLEVEL1  in abilities:
+                if self.can_afford(RESEARCH_ZERGGROUNDARMORLEVEL1):
+                    self.combinedActions.append(evo(RESEARCH_ZERGGROUNDARMORLEVEL1))
+            
+            elif evo.noqueue and len(abilities) > 0 and RESEARCH_ZERGMISSILEWEAPONSLEVEL1 in abilities:
+                if self.can_afford(RESEARCH_ZERGMISSILEWEAPONSLEVEL1):
+                    self.combinedActions.append(evo(RESEARCH_ZERGMISSILEWEAPONSLEVEL1))
+            
+            elif evo.noqueue and len(abilities) > 0 and RESEARCH_ZERGGROUNDARMORLEVEL2 in abilities:
+                if self.can_afford(RESEARCH_ZERGGROUNDARMORLEVEL2):
+                    self.combinedActions.append(evo(RESEARCH_ZERGGROUNDARMORLEVEL2))
+            
+            elif evo.noqueue and len(abilities) > 0 and RESEARCH_ZERGMISSILEWEAPONSLEVEL2 in abilities:
+                if self.can_afford(RESEARCH_ZERGMISSILEWEAPONSLEVEL2):
+                    self.combinedActions.append(evo(RESEARCH_ZERGMISSILEWEAPONSLEVEL2))
+            
+            elif evo.noqueue and len(abilities) > 0 and RESEARCH_ZERGGROUNDARMORLEVEL3 in abilities:
+                if self.can_afford(RESEARCH_ZERGGROUNDARMORLEVEL3):
+                    self.combinedActions.append(evo(RESEARCH_ZERGGROUNDARMORLEVEL3))
+            
+            elif evo.noqueue and len(abilities) > 0 and RESEARCH_ZERGMISSILEWEAPONSLEVEL3 in abilities:
+                if self.can_afford(RESEARCH_ZERGMISSILEWEAPONSLEVEL3):
+                    self.combinedActions.append(evo(RESEARCH_ZERGMISSILEWEAPONSLEVEL3))
+        
+        # Overlord speed
 
 
     async def expand_now(self, building: UnitTypeId=None, max_distance: Union[int, float]=10, location: Optional[Point2]=None):
@@ -333,73 +370,28 @@ class LingLoverBot(BaseBot):
 #                continue
 
             # Do we have an army advantage?
-            if army_advantage > 0:
+            if army_advantage > 0 or unit.distance_to(home_location) < 6:
                 # We have a larger army. Engage enemy
                 attack_position = nearby_enemy_units.closest_to(unit).position
+                possible_targets = nearby_enemy_units.in_attack_range_of(unit)
+                targets = possible_targets.sorted(lambda u: u.ground_dps + 10/u.health_percentage, True)
 
-                # If not already attacking, attack
-                if not self.has_order(ATTACK, unit) or not self.has_target(attack_position, unit):
-                    self.combinedActions.append(unit.attack(attack_position))
-                
-                # Activate guardian shield for sentries (if enemy army value is big enough)
-#                if has_guardianshield and enemy_army_value > 200:
-#                    await self.order(unit, GUARDIANSHIELD_GUARDIANSHIELD)
-            else:
-                # Others can move normally
-                if not self.has_order(MOVE, unit):
-                    self.combinedActions.append(unit.move(home_location))
+                # Only micro kite inwards if x% bigger army
+                if unit.weapon_cooldown > 0 and friendly_army_value - enemy_army_value * 1.10 > 0:
+                    if len(targets) > 0:
+                        self.combinedActions.append(unit.move(targets[0].position))
+                    else:
+                        self.combinedActions.append(unit.move(attack_position))
 
-    async def micro():
-        # Micro for each individual army unit
-        for unit in army_units:
-            # Find nearby enemy units
-            nearby_enemy_units = self.remembered_enemy_units.not_structure.filter(lambda unit: unit.type_id not in self.units_to_ignore).closer_than(15, unit)
+                else:
 
-            # If we don't have any nearby enemies
-            if not nearby_enemy_units.exists:
-                # If we don't have an attack order, cast one now
-                if not self.has_order(ATTACK, unit) or (self.known_enemy_units.exists and not self.has_target(attack_location, unit)):
-                    if attack_random_exp:
-                        # If we're attacking a random exp, find one now
-                        random_exp_location = random.choice(list(self.expansion_locations.keys()))
-                        self.combinedActions.append(unit.attack(random_exp_location))
-                        #print("Attack random exp")
-                    elif unit.distance_to(attack_location) > 10:
-                        self.combinedActions.append(unit.attack(attack_location))
-                        #print("Attack no enemy nearby")
-                
-                continue # Do no further micro
+                    if len(targets) > 0:
+                        self.combinedActions.append(unit.attack(targets[0]))
 
-            # Calculate friendly vs enemy army value
-            friendly_army_value = self.friendly_army_value(unit, 10) #20
-            enemy_army_value = self.enemy_army_value(nearby_enemy_units.closest_to(unit), 10) #30
-            army_advantage = friendly_army_value - enemy_army_value
-
-            # If our shield is low, escape a little backwards
-#            if unit.is_taking_damage and unit.shield < 20 and unit.type_id not in [ZEALOT]:
-#                escape_location = unit.position.towards(home_location, 4)
-#                if has_blink:
-#                    # Stalkers can blink
-#                    await self.order(unit, EFFECT_BLINK_STALKER, escape_location)
-#                else:
-                    # Others can move normally
-#                    if not self.has_order(MOVE, unit):
-#                        self.combinedActions.append(unit.move(escape_location))
-
-#                continue
-
-            # Do we have an army advantage?
-            if army_advantage > 0:
-                # We have a larger army. Engage enemy
-                attack_position = nearby_enemy_units.closest_to(unit).position
-
-                # If not already attacking, attack
-                if not self.has_order(ATTACK, unit) or not self.has_target(attack_position, unit):
-                    self.combinedActions.append(unit.attack(attack_position))
-                
-                # Activate guardian shield for sentries (if enemy army value is big enough)
-#                if has_guardianshield and enemy_army_value > 200:
-#                    await self.order(unit, GUARDIANSHIELD_GUARDIANSHIELD)
+                    # If not already attacking, attack
+                    elif not self.has_order(ATTACK, unit) or not self.has_target(attack_position, unit):
+                        self.combinedActions.append(unit.attack(attack_position))
+                    
             else:
                 # Others can move normally
                 if not self.has_order(MOVE, unit):
